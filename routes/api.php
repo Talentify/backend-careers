4966ee27cd42fdb32c9f31a1d3c63471b7fc90c8
Thiago <thiago.carnaes@gmail.com>
2020-02-10 10:46:30 -0300

    add middlewarre to set headers in requests

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/', 'Controller@root');
+Route::middleware(['request-json'])->get('/', 'Controller@root');
 
 Route::fallback('Controller@fallback');
 
@@ -21,19 +21,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::name('jobs.')->prefix('jobs')->get('/', 'JobController@index')->name('list');
-Route::name('job.')->prefix('job')->get('/{jobId}', 'JobController@show')->name('view');
+Route::name('jobs.')->prefix('jobs')->middleware(['request-json'])->get('/', 'JobController@index')->name('list');
+Route::name('job.')->prefix('job')->middleware(['request-json'])->get('/{jobId}', 'JobController@show')->name('view');
 
-Route::name('job.')->prefix('job')->middleware(['auth:api', 'rbac'])->group(function () {
+Route::name('job.')->prefix('job')->middleware(['auth:api', 'rbac', 'request-json'])->group(function () {
     Route::post('/', 'JobController@store')->name('insert');
     Route::put('/{jobId}', 'JobController@update')->name('update');
     Route::delete('/{jobId}', 'JobController@destroy')->name('delete');
 });
 
-Route::name('users.')->prefix('users')->middleware(['auth:api', 'rbac'])->get('/', 'UserController@index')->name('list');
-Route::name('user.')->prefix('user')->post('/', 'UserController@store')->name('insert');
+Route::name('users.')->prefix('users')->middleware(['auth:api', 'rbac', 'request-json'])->get('/', 'UserController@index')->name('list');
+Route::name('user.')->prefix('user')->middleware(['request-json'])->post('/', 'UserController@store')->name('insert');
 
-Route::name('user.')->prefix('user')->middleware(['auth:api', 'rbac'])->group(function () {
+Route::name('user.')->prefix('user')->middleware(['auth:api', 'rbac', 'request-json'])->group(function () {
     Route::get('/{userId}', 'UserController@show')->name('view');
     Route::put('/{userId}', 'UserController@update')->name('update');
     Route::delete('/{userId}', 'UserController@destroy')->name('delete');
