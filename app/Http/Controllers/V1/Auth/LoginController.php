da914bd8ade7dbcc12d8c754c8ac533c459fee83
Rodrigo Roque <rodrigogoncalvesroque@gmail.com>
2020-02-21 22:02:56 -0300

    Update Responses

--- app/Http/Controllers/V1/Auth/LoginController.php
+++ app/Http/Controllers/V1/Auth/LoginController.php
@@ -8,6 +8,8 @@ use App\Core\Http\Controllers\Controller;
 use App\Core\Http\Requests\Request;
 use App\Http\Requests\V1\Auth\Login;
 use App\Services\V1\Auth\LoginService;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
 
 /**
  * Class LoginController
@@ -23,17 +25,25 @@ class LoginController extends Controller
         $this->loginService = $loginService;
     }
 
+    /**
+     * @param  Login  $request
+     *
+     * @return JsonResponse
+     * @throws AuthenticationException
+     */
     public function login(Login $request)
     {
         $token = $this->loginService->login($request);
 
-        return response()->success($token);
+        return response()->json($token);
     }
 
     public function logout(Request $request)
     {
         $this->loginService->logout($request);
 
-        return response()->success(__('auth.logout'));
+        return response()->json([
+            'message' => __('auth.logout'),
+        ]);
     }
 }
