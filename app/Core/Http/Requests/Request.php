526993bad06c2265957930f4be5a65c4d7dd9c67
Rodrigo Roque <rodrigogoncalvesroque@gmail.com>
2020-02-21 22:02:24 -0300

    Remove Abstract

--- app/Core/Http/Requests/Request.php
+++ app/Core/Http/Requests/Request.php
@@ -6,7 +6,7 @@ namespace App\Core\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-abstract class Request extends FormRequest
+class Request extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
