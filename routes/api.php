75f1db17fac38d138048413bc5043ce937101cf9
Rodrigo Roque <rodrigogoncalvesroque@gmail.com>
2020-02-21 22:15:46 -0300

    Remove Routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\V1\Auth\LoginController;
-use App\Http\Controllers\V1\UserController;
 
 /**
  * Guest routes
@@ -13,29 +12,4 @@ Route::post('auth/login', [LoginController::class, 'login'])->name('auth.login')
  */
 Route::middleware('auth:api')->group(function () {
     Route::post('auth/logout', [LoginController::class, 'logout'])->name('auth.logout');
-
-    Route::prefix('users')
-        ->name('users.')
-        ->group(function () {
-            Route::get('', [UserController::class, 'index'])
-                ->name('index');
-
-            Route::get('count', [UserController::class, 'index'])
-                ->name('count');
-
-            Route::post('', [UserController::class, 'store'])
-                ->name('store');
-
-            Route::get('{id}', [UserController::class, 'show'])
-                ->where('id', '[0-9]+')
-                ->name('show');
-
-            Route::patch('{id}', [UserController::class, 'update'])
-                ->where('id', '[0-9]+')
-                ->name('update');
-
-            Route::delete('{id}', [UserController::class, 'destroy'])
-                ->where('id', '[0-9]+')
-                ->name('destroy');
-        });
 });
