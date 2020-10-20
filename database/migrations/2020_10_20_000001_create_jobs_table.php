fb6fd3d4e45beadffc91220a55f91e55c3b82994
Pablo Matheus <pablomatheus.ti@gmail.com>
2020-10-20 08:48:34 -0300

    migratioN

---
+++ database/migrations/2020_10_20_000001_create_jobs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Schema table name to migrate
+     * @var string
+     */
+    public $tableName = 'jobs';
+
+    /**
+     * Run the migrations.
+     * @table jobs
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->enum('status', ['opened', 'closed', 'paused'])->default('opened');
+            $table->string('workplace')->nullable()->default(null);
+            $table->decimal('salary', 10, 2)->nullable()->default(null);
+            $table->softDeletes();
+            $table->nullableTimestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       Schema::dropIfExists($this->tableName);
+     }
+}
