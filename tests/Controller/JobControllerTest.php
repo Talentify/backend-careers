9e061dc1efbb4e107c24a9e3183db90350265bea
aironou <aironou@aironou.com.br>
2020-11-19 11:10:45 -0300

    Create App\Tests\Controller\JobControllerTest

---
+++ tests/Controller/JobControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\HttpFoundation\Response;
+
+class JobControllerTest extends WebTestCase
+{
+    /**
+     * @var KernelBrowser
+     */
+    private KernelBrowser $kernelBrowser;
+
+    public function setUp(): void
+    {
+        $this->kernelBrowser = $this->createClient();
+        $application = new Application($this->kernelBrowser->getKernel());
+        $application->setAutoExit(false);
+        $application->run(new StringInput('doctrine:database:drop --force --quiet'));
+        $application->run(new StringInput('doctrine:database:create --quiet'));
+        $application->run(new StringInput('doctrine:migrations:migrate --no-interaction --quiet'));
+    }
+
+    public function testReadWithoutResults(): void
+    {
+        $this->kernelBrowser->request('GET', '/job');
+        $response = $this->kernelBrowser->getResponse();
+        $this->assertJson($response->getContent());
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+    }
+
+    /**
+     * @return array
+     */
+    public function validCreateProvider(): array
+    {
+        $completeData = [
+            'title' => 'title',
+            'description' => 'description',
+            'status' => true,
+            'workplace' => [
+                'address' => 'address',
+                'city' => 'city',
+                'state' => 'state',
+                'country' => 'country'
+            ],
+            'salary' => 100.1
+        ];
+        return [
+            'complete job' => [json_encode($completeData)],
+            'without workplace' => [json_encode(array_merge($completeData, ['workplace' => null]))],
+            'without salary' => [json_encode(array_merge($completeData, ['salary' => null]))]
+        ];
+    }
+
+    /**
+     * @param string $data
+     *
+     * @dataProvider validCreateProvider
+     */
+    public function testCreate(string $data): void
+    {
+        $this->kernelBrowser->request('POST', '/job', [], [], ['CONTENT_TYPE' => 'application/json'], $data);
+        $response = $this->kernelBrowser->getResponse();
+        $this->assertJson($response->getContent());
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->kernelBrowser->request('GET', '/job');
+        $response = $this->kernelBrowser->getResponse();
+        $this->assertJson($response->getContent());
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    }
+}
