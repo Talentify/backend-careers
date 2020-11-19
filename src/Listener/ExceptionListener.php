2414a8080330d8d5b715ee16d49a177bfac7f8e7
aironou <aironou@aironou.com.br>
2020-11-18 22:45:45 -0300

    Create App\Listener\ExceptionListener

---
+++ src/Listener/ExceptionListener.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Listener;
+
+use App\Exceptions\EmptyException;
+use App\Exceptions\TooLongException;
+use Doctrine\ORM\NoResultException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class ExceptionListener
+{
+    /**
+     * @param ExceptionEvent $event
+     */
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $event->setResponse($this->handleThrowable($event->getThrowable()));
+    }
+
+    /**
+     * @param \Throwable $throwable
+     * @return JsonResponse
+     */
+    private function handleThrowable(\Throwable $throwable): JsonResponse
+    {
+        try {
+            throw $throwable;
+        } catch (NoResultException|EmptyException $exception) {
+            return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        } catch (TooLongException|\TypeError $exception) {
+            return new JsonResponse(null, Response::HTTP_BAD_REQUEST);
+        } catch (\Throwable $throwable) {
+            return new JsonResponse(null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
