810876237f503ec172d3252e2b937bc85913946c
aironou <aironou@aironou.com.br>
2020-11-22 16:02:04 -0300

    Add new exception in ExceptionListenerTest->exceptionsProvider()

--- src/Listener/ExceptionListener.php
+++ src/Listener/ExceptionListener.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\NoResultException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ExceptionListener
 {
@@ -30,6 +31,8 @@ class ExceptionListener
             return new JsonResponse(null, Response::HTTP_NOT_FOUND);
         } catch (TooLongException|\TypeError $exception) {
             return new JsonResponse(null, Response::HTTP_BAD_REQUEST);
+        } catch (HttpException $exception) {
+            return new JsonResponse(null, $exception->getStatusCode());
         } catch (\Throwable $throwable) {
             return new JsonResponse(null, Response::HTTP_INTERNAL_SERVER_ERROR);
         }
--- tests/Listener/ExceptionListenerTest.php
+++ tests/Listener/ExceptionListenerTest.php
@@ -5,11 +5,17 @@ use App\Exceptions\EmptyException;
 use App\Exceptions\TooLongException;
 use App\Listener\ExceptionListener;
 use Doctrine\ORM\NoResultException;
+use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class ExceptionListenerTest extends WebTestCase
 {
@@ -29,11 +35,15 @@ class ExceptionListenerTest extends WebTestCase
     public function exceptionsProvider(): array
     {
         return [
-            'empty exception' => [EmptyException::class, Response::HTTP_NOT_FOUND],
-            'too long exception' => [TooLongException::class, Response::HTTP_BAD_REQUEST],
-            'no result exception' => [NoResultException::class, Response::HTTP_NOT_FOUND],
-            'type error' => [\TypeError::class, Response::HTTP_BAD_REQUEST],
-            'throwable' => [\Throwable::class, Response::HTTP_INTERNAL_SERVER_ERROR]
+            'empty exception (404)' => [EmptyException::class, Response::HTTP_NOT_FOUND],
+            'too long exception (400)' => [TooLongException::class, Response::HTTP_BAD_REQUEST],
+            'no result exception (404)' => [NoResultException::class, Response::HTTP_NOT_FOUND],
+            'type error (400)' => [\TypeError::class, Response::HTTP_BAD_REQUEST],
+            'throwable (500)' => [\Throwable::class, Response::HTTP_INTERNAL_SERVER_ERROR],
+            'http exception (400)' => [BadRequestHttpException::class, Response::HTTP_BAD_REQUEST],
+            'http exception (401)' => [UnauthorizedHttpException::class, Response::HTTP_UNAUTHORIZED],
+            'http exception (403)' => [AccessDeniedHttpException::class, Response::HTTP_FORBIDDEN],
+            'http exception (404)' => [NotFoundHttpException::class, Response::HTTP_NOT_FOUND],
         ];
     }
 
@@ -49,10 +59,25 @@ class ExceptionListenerTest extends WebTestCase
             $this->createKernel(),
             Request::createFromGlobals(),
             0,
-            $this->createMock($exception)
+            $this->getExceptionMock($exception, $expected)
         );
         $this->exceptionListener->onKernelException($exceptionEvent);
         $this->assertInstanceOf(JsonResponse::class, $exceptionEvent->getResponse());
         $this->assertEquals($exceptionEvent->getResponse()->getStatusCode(), $expected);
+    }
+
+    /**
+     * @param string $exceptionClass
+     * @param int $statusCode
+     * @return MockObject
+     */
+    private function getExceptionMock(string $exceptionClass, int $statusCode): MockObject
+    {
+        $exception = $this->createMock($exceptionClass);
+        if (in_array(HttpException::class, class_parents($exceptionClass))) {
+            $exception->method('getStatusCode')
+                ->will($this->returnValue($statusCode));
+        }
+        return $exception;
     }
 }
