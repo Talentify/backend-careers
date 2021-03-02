b6b0f7df16897a7dbdcfcd95d7b356efce1b5569
Paulo Arguelo <parguelo@grupoimagetech.com.br>
2021-03-02 15:04:04 -0400

    Adição de migration para tabela Positions

---
+++ config/Migrations/20210302185914_CreatePositions.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreatePositions extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('positions');
+        $table->addColumn('title', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('description', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('status', 'boolean', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('address', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('salary', 'decimal', [
+            'default' => null,
+            'null' => false,
+            'precision' => 10,
+            'scale' => 6,
+        ]);
+        $table->addColumn('company', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('user_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+
+        $table->addForeignKey('user_id', 'users', ['id'],
+            ['constraint' => 'users_fk']);
+
+
+        $table->create();
+    }
+}
