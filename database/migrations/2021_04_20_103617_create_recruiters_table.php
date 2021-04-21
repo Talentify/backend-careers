3712bd802e2492a2533d7cd5bae1dcb13d6ddedd
Rander Carlos <randerccf@gmail.com>
2021-04-21 17:37:09 -0300

    create migrations. delete unused files

--- database/migrations/2021_04_20_103617_create_recruiters_table.php
+++ database/migrations/2021_04_20_103617_create_recruiters_table.php
@@ -17,10 +17,10 @@ class CreateRecruitersTable extends Migration
             $table->id();
             $table->string('name');
             $table->date('birth');
-            $table->string('cpf');
+            $table->string('cpf')->unique();
             $table->string('company');
             $table->string('address');
-            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('user_id')->unique();
 
             $table->foreign('user_id')->references('id')->on('users')
                 ->onUpdate('cascade')->onDelete('cascade');
